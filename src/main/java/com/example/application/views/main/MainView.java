da98d785d9d9a2603cbe4366890d402b04af60a6
Luke Brown <Luketheduke48@gmail.com>
2023-07-10 18:00:21 -0400

    Front end stuff

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.messages.MessageInput;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -18,34 +19,41 @@ public class MainView extends HorizontalLayout {
     public MainView() {
     	
     	String primaryColor  = "#303030";
-    	String secondaryColor  = "#232424";
+    	String secondaryColor  = "#c6cccc";
     	String tertiaryColor  = "#6ec2c2";
 
     	
 
-    	getStyle().set("background-color", primaryColor);
+    	//getStyle().set("background-color", primaryColor);
     	
     	VerticalLayout promptLayout = new VerticalLayout();
        	promptLayout.setWidth("20em");
     	promptLayout.getStyle().set("background-color", secondaryColor);
     	
-    	Button primaryButton = new Button(" +  New Chat ");
-    	primaryButton.getStyle().set("background-color", tertiaryColor);
-    	primaryButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-    	primaryButton.setWidthFull();
-    	promptLayout.add(primaryButton);
+    	Button newChatButton = new Button(" +  New Chat ");
+    	//newChatButton.getStyle().set("background-color", tertiaryColor);
+    	newChatButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    	newChatButton.setWidthFull();
+    	promptLayout.add(newChatButton);
 
     	VerticalLayout chatBotLayout = new VerticalLayout();
+    	chatBotLayout.setJustifyContentMode(JustifyContentMode.END);
     	
     	MessageInput input = new MessageInput();
     	input.addSubmitListener(submitEvent -> {
     	    Notification.show("Message received: " + submitEvent.getValue(),
     	            3000, Notification.Position.MIDDLE);
     	});
-    	input.getStyle().set("background-color", tertiaryColor);
+    	
     	input.setWidthFull();
     	chatBotLayout.add(input);
- 
+    	
+    	HorizontalLayout searchBarLayout = new HorizontalLayout();
+    	searchBarLayout.setJustifyContentMode(JustifyContentMode.END);
+    	
+    	
+
+    	chatBotLayout.add(searchBarLayout);
     	
     	
     	add(promptLayout);
